ef537caeff72e1d69a0ec8cc638686fb11d5fb5c
yanaept-creator <yanaept@gmail.com>
2026-03-05 19:00:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from gui_app import AudioApp
 
-root = tk.Tk()
-app = AudioApp(root)
-root.geometry("1200x550")
-root.mainloop()
+root = tk.Tk()  #tworzy główne okno aplikacji, zmienna root przechowuje to okno, w którym będą wszystkie widżety (przyciski, wykresy, suwaki).
+app = AudioApp(root) #obiekt klasy AudioApp
+root.geometry("1200x550") # rozmiar okna giu
+root.mainloop()  #uruchamia pętlę zdarzeń, dzięki której aplikacja reaguje na kliknięcia przycisków, wpisywanie tekstu, rysowanie wykresów itp.
+
 
